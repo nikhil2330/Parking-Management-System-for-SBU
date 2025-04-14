a6c2984d10ae308da5f6b8ce0d71364b843fd5ff
Nikhil233021 <nikhil.sundaresan@stonybrook.edu>
2025-04-14 11:44:37 -0400

    app.js which works

--- server/App.js
+++ server/App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../config/.env') });
+require('dotenv').config({ path: './config/.env' });
 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
